96eb5eecd18c404b356d85d8afa806b5d2137b7c
JIAWEI LYU <ljw960926@gmail.com>
2020-02-18 11:18:59 -0600

    update train script

--- FCONV/train.sh
+++ FCONV/train.sh
@@ -6,8 +6,9 @@ TRAIN=trainings/wmt18_en_zh
 mkdir -p $TRAIN/fconv
 CUDA_VISIBLE_DEVICES=0 fairseq-train $DATADIR --memory-efficient-fp16\
   --arch fconv --mu 1000000 --me 50 \
-  --batch-size 16 --max-tokens 2048 \
-  --lr 0.5 --clip-norm 0.1 --dropout 0.2 --min-lr 1e-09\
+  --max-tokens 2048 \
+  --lr 0.00005 --clip-norm 0.1 --dropout 0.2 --min-lr 1e-09\
+  --update-freq 1 \
   --criterion label_smoothed_cross_entropy --label-smoothing 0.1 \
   --lr-scheduler fixed --force-anneal 50 \
   --no-epoch-checkpoints --save-interval-updates 100000 --keep-interval-updates 1\
