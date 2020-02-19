c3e08d1a3eb2c9b95694353e8dd5de3a7f1465de
JIAWEI LYU <ljw960926@gmail.com>
2020-02-18 19:54:33 -0600

    update translate script

--- FCONV/interactive.sh
+++ FCONV/interactive.sh
@@ -1,4 +1,5 @@
 DATADIR=data-bin/wmt18_en_zh
+BPEDIR=data/wmt18_en_zh
 MODEL=trainings/wmt18_en_zh/fconv/checkpoint_best.pt
 
 fairseq-interactive $DATADIR \
@@ -7,6 +8,7 @@ fairseq-interactive $DATADIR \
   -s en -t zh \
   --tokenizer moses \
   --bpe subword_nmt \
+  --bpe-codes $BPEDIR/codes.32000.bpe.en \
   --remove-bpe \
   --print-alignment \
   --beam 10
