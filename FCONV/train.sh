c636f11713896784d2a7df23a94ea29fce18be16
JIAWEI LYU <ljw960926@gmail.com>
2020-02-23 22:39:36 -0600

    update train script

--- FCONV/train.sh
+++ FCONV/train.sh
@@ -14,15 +14,31 @@ mkdir -p $TRAIN/fconv
 #   --no-epoch-checkpoints --save-interval-updates 100000 --keep-interval-updates 1\
 #   --save-dir $TRAIN/fconv \
 
-CUDA_VISIBLE_DEVICES=0 fairseq-train $DATADIR \
+#CUDA_VISIBLE_DEVICES=0 fairseq-train $DATADIR \
+#  --clip-norm 0 --optimizer adam --lr 0.0005 \
+#  --source-lang en --target-lang zh --max-tokens 2048 \
+#  --max-tokens 2048 \
+#  --min-lr '1e-09' --weight-decay 0.0001 \
+#  --criterion label_smoothed_cross_entropy --label-smoothing 0.1 \
+#  --lr-scheduler inverse_sqrt \
+#  --ddp-backend=no_c10d \
+#  --max-update 500000 --warmup-updates 4000 --warmup-init-lr '1e-07' \
+#  --no-epoch-checkpoints --save-interval-updates 100000 --keep-interval-updates 1\
+#  --adam-betas '(0.9, 0.98)' --keep-last-epochs 2 \
+#  --dropout 0.3 --attention-dropout 0.1 --weight-dropout 0.1 \
+#  -a lightconv_wmt_zh_en_big --save-dir $TRAIN/fconv \
+#  --encoder-conv-type lightweight --decoder-conv-type lightweight \
+#  --encoder-glu 0 --decoder-glu 0
+
+CUDA_VISIBLE_DEVICES=0 fairseq-train $DATADIR --memory-efficient-fp16 \
   --clip-norm 0 --optimizer adam --lr 0.0005 \
-  --source-lang en --target-lang zh --max-tokens 2048 \
-  --max-tokens 2048 \
+  --source-lang zh --target-lang en --max-tokens 2048 \
   --min-lr '1e-09' --weight-decay 0.0001 \
   --criterion label_smoothed_cross_entropy --label-smoothing 0.1 \
   --lr-scheduler inverse_sqrt \
   --ddp-backend=no_c10d \
-  --max-update 500000 --warmup-updates 4000 --warmup-init-lr '1e-07' \
+  --max-epoch 5 \
+  --max-update 10000 --warmup-updates 4000 --warmup-init-lr '1e-07' \
   --no-epoch-checkpoints --save-interval-updates 100000 --keep-interval-updates 1\
   --adam-betas '(0.9, 0.98)' --keep-last-epochs 2 \
   --dropout 0.3 --attention-dropout 0.1 --weight-dropout 0.1 \
